d2d479bbeaf3a3b0f4f1d41c8129e8c6fe6baacc
ArcaneArsenic <tclevi@ksu.edu>
2020-07-17 05:01:53 -0500

    a

--- lab-26-assignment-ArcaneArsenic-e593e6560214e4747db0be35434e3ceca5a4da6a/Ksu.Cis300.HuffmanTrees/UserInterface.cs
+++ lab-26-assignment-ArcaneArsenic-e593e6560214e4747db0be35434e3ceca5a4da6a/Ksu.Cis300.HuffmanTrees/UserInterface.cs
@@ -36,10 +36,44 @@ namespace Ksu.Cis300.HuffmanTrees
         /// <param name="fn"></param>
         /// <returns></returns>
         private long[] freqTableBuilder(string fn)
-        { 
-        
+        {
+            long[] chars = new long[256];
+            int i = 0;
+            using (FileStream fs = File.OpenRead(fn))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                    chars[i] = b[i];
+                    i++;
+                }
+            }
+            return chars;
         }
 
+        /// <summary>
+        /// do not iterate with a byte
+        /// set priority to freq table equivalent 
+        /// check zero, create node
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        private MinPriorityQueue<long, BinaryTreeNode<byte>> HuffBuild(long[] x)
+        {
+            x = freqTableBuilder(Console.ReadLine());
+            MinPriorityQueue<long, BinaryTreeNode<byte>> mpq = new 
+                MinPriorityQueue<long, BinaryTreeNode<byte>>();
+            while (x.Length > mpq.Count)
+            {
+                BinaryTreeNode<byte> node = new BinaryTreeNode<byte>(Convert.ToByte(x), null, null);
+                mpq.Add(x[0], node);
+            }
+            return mpq;
+
+        }
         /// <summary>
         /// Handles a Click event on the "Select a File" button.
         /// </summary>
@@ -47,7 +81,22 @@ namespace Ksu.Cis300.HuffmanTrees
         /// <param name="e"></param>
         private void uxSelectFile_Click(object sender, EventArgs e)
         {
+            if (uxOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    BinaryTreeNode<byte> t = null;
 
+                    // Add code to build the Huffman tree and assign it to t.
+                    HuffBuild(freqTableBuilder(Convert.ToString(sender)));
+
+                    new TreeForm(t, 100).Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
         }
     }
 }
